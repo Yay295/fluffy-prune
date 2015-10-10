f10d2a337eacdb8f07b629ff14337c4be595937c
Yay295 <Yay295@users.noreply.github.com>
2015-10-10 00:03:10 -0600

    Update quadtree.cpp
    I forgot templated stuff all has to be in the same file.
    Merged quadtree.h and quadtree.cpp into quadtree.cpp

--- quadtree.cpp
+++ quadtree.cpp
@@ -1,4 +1,84 @@
-#include "quadtree.h"
+#pragma once
+
+#ifndef QUADTREE_H
+#define QUADTREE_H
+
+
+/******************************************************************************
+@author Jeremy Gamet and John Colton
+
+@par Description:
+This class is our implementation of a quadtree.
+******************************************************************************/
+template<class datatype>
+class quadtree
+{
+	private:
+
+	/**************************************************************************
+	@author Jeremy Gamet and John Colton
+
+	@par Description:
+	This struct holds the data in our quadtree.
+	**************************************************************************/
+	struct point
+	{
+		point();
+		point( const size_t X, const size_t Y, const datatype & D );
+		datatype getData() const;
+
+		const bool isPoint = true; // This point is a point.
+		size_t x = 0, y = 0; // The location of this point.
+		datatype data; // THe data at this point.
+	};
+
+	/**************************************************************************
+	@author Jeremy Gamet and John Colton
+
+	@par Description:
+	This struct handles the division of points.
+	**************************************************************************/
+	struct quad : point
+	{
+		quad( const size_t X, const size_t Y, const size_t W, const size_t H );
+
+		bool contains( const size_t x, const size_t y );
+		void insert( const point * const P );
+		void insert( const size_t X, const size_t Y, const datatype & D );
+
+		// Top Left, Top Right, Bottom Left, Bottom Right
+		point * tl = nullptr;
+		point * tr = nullptr;
+		point * bl = nullptr;
+		point * br = nullptr;
+
+		// This quad is not a point.
+		const bool isPoint = false;
+
+		// The point defining the top left of this quad.
+		size_t x = 0, y = 0;
+
+		// The horizontal and vertical ranges of points this quad contains.
+		size_t width, height;
+
+		// The data in this quad.
+		datatype data;
+	};
+
+	quad * root = nullptr;
+
+	// The horizontal and vertical ranges of points this quadtree contains.
+	size_t width, height;
+
+
+	public:
+
+	quadtree( const size_t W, const size_t H );
+	~quadtree();
+	bool insert( const size_t X, const size_t Y, const datatype & D );
+	void subdiv();
+	void clear();
+};
 
 
 template<class datatype>
@@ -6,7 +86,9 @@ quadtree<datatype>::point::point() {}
 
 template<class datatype>
 quadtree<datatype>::point::point( const size_t X, const size_t Y, const datatype & D )
-	: x( X ), y( Y ), data( D ) {}
+	: x( X ), y( Y ), data( D )
+{
+}
 
 template<class datatype>
 datatype quadtree<datatype>::point::getData() const { return data; }
@@ -14,7 +96,9 @@ datatype quadtree<datatype>::point::getData() const { return data; }
 
 template<class datatype>
 quadtree<datatype>::quad::quad( const size_t X, const size_t Y, const size_t W, const size_t H )
-	: isPoint( false ), x( X ), y( Y ), width( W ), height( H ) {}
+	: isPoint( false ), x( X ), y( Y ), width( W ), height( H )
+{
+}
 
 template<class datatype>
 bool quadtree<datatype>::quad::contains( const size_t X, const size_t Y )
@@ -128,7 +212,7 @@ quadtree<datatype>::quadtree( const size_t W, const size_t H ) : width( W ), hei
 
 @par Description:
 Destructor for the quadtree class.
-*****************************************************************************/
+******************************************************************************/
 template <class datatype>
 quadtree<datatype>::~quadtree()
 {
@@ -168,7 +252,7 @@ bool quadtree<datatype>::insert( const size_t X, const size_t Y, const datatype 
 
 @par Description:
 Insert method for the quadtree class.
-*****************************************************************************/
+******************************************************************************/
 template <class datatype>
 void quadtree<datatype>::subdiv()
 {
@@ -180,9 +264,12 @@ void quadtree<datatype>::subdiv()
 
 @par Description:
 Insert method for the quadtree class.
-*****************************************************************************/
+******************************************************************************/
 template <class datatype>
 void quadtree<datatype>::clear()
 {
 	//TBD
 }
+
+
+#endif /* QUADTREE_H */
